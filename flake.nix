74d5c4a733839c254e8eec3d848d8de4d5641b71
Mutsuha Asada <me@momee.mt>
2023-12-16 16:31:51 +0900

    add nix-shell config

--- flake.nix
+++ flake.nix
@@ -1,30 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }:
+  outputs = { self, nixpkgs, fenix, flake-utils, flake-compat, ... }:
     flake-utils.lib.eachDefaultSystem (
       system: let
+        overlays = [
+          (_: super: let pkgs = fenix.inputs.nixpkgs.legacyPackages.${super.system}; in fenix.overlays.default pkgs pkgs)
+        ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
-      in rec {
+      in {
         devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
-            rust-analyzer-unwrapped
-            rustup
-            clippy
-            rustfmt
-            gcc
-            libiconv
+          buildInputs = with pkgs; [
+            nil
+            openssl
+            glib
+            pkg-config
+            (fenix.packages.${system}.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer-nightly
           ];
+          RUST_SRC_PATH = "${fenix.packages.${system}.complete.rust-src}/lib/rustlib/src/rust/library";
         };
       }
     );
